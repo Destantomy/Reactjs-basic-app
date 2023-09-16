c8a447a3a3049ef17edf575ae24456954b446661
Destanto <destantomy8@gmail.com>
2023-09-16 11:47:08 +0700

    handling fetch error

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,33 @@ const Home = () => {
 // hook declare start
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 // /hook declare start
 
     useEffect(() => {
       setTimeout(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
+          if(!res.ok) {
+            throw Error('couldnt fetch data from resource')
+          }
           return res.json();
         })
         .then(data => {
-          console.log(data);
           setBlogs(data);
           setIsPending(false);
-        });
+          setError(null)
+        })
+        .catch(err => {
+          setIsPending(false);
+          setError(err.message);
+        })
       }, 1000);
     }, []);
 
   return (
     <div className="home">
+        {error && <div>{error}</div>}
         {isPending && <div>Loading ..</div>}
         {blogs && <Bloglist blogs={blogs} title='All Games'/>}
     </div>
