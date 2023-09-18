c8b1067b3b573a87213731336d8d6435cae6d5be
Destanto <destantomy8@gmail.com>
2023-09-18 15:37:14 +0700

    controlled inputs (forms)

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+  const [title, setTitle] = useState('')
+  const [auth, setAuth] = useState('desta')
+  const [studio, setStudio] = useState('')
+  const [detail, setDetail] = useState('')
+
   return (
-    <div>
-      <p>New blog</p>
+    <div className='create'>
+      <p>New Game Disk</p>
+      <form>
+        <label>Game title: </label>
+        <input type="text" required autoFocus
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Author: </label>
+        <select required
+        value={auth}
+        onChange={(e) => setAuth(e.target.value)}>
+          <option value="desta">desta</option>
+          <option value="destajr">destajr</option>
+        </select>
+        <label>Studio: </label>
+        <input type="text" required
+        value={studio}
+        onChange={(e) => setStudio(e.target.value)}
+        />
+        <label>Detail: </label>
+        <textarea required
+        value={detail}
+        onChange={(e) => setDetail(e.target.value)}
+        ></textarea>
+        <button>Add BLog</button>
+      </form>
+
+      
+      <p><br /> Testing Area here onChange function</p>
+      <p>{title}</p>
+      <p>{auth}</p>
+      <p>{studio}</p>
+      <p>{detail}</p>
     </div>
   )
 }
